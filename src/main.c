8bcaa037a41e6965153364734aca51389a416a21
Sacha Senas <sachasenas@gmail.com>
2025-05-09 01:07:58 +0200

    lazy

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main(void)
 	cam.transform = view_transform(point(0, 2, -10), point(1, 0, 0), vector(0, 1, 0));
 
 
-	canvas image = low_render(cam, w, 1);
+	canvas image = low_render(cam, w, 5);
 	canvas_to_ppm(&image, "scene.ppm");
 	destroy_canvas(&image);
 	destroy_world(&w);
